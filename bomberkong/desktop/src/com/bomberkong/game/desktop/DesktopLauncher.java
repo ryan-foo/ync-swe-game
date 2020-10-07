ad45f0273e0c133d5dbc99b9aa4d9cf4f75d7832
Ryan <ryanicale@gmail.com>
2020-10-07 18:25:53 +0800

    Revert "initial commit"
    This reverts commit 82c4605368dd7c444cb7609abe09020f4f06f42f.

---
+++ bomberkong/desktop/src/com/bomberkong/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bomberkong.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bomberkong.game.BomberKong;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BomberKong(), config);
+	}
+}
